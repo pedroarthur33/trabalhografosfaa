b317322c43c5992f12ab95a70af137a06f50b352
eduardohabitzreuter@gmail.com <eduardohabitzreuter@gmail.com@89243517-489e-33d1-b2b3-005f8cf9e537>
2012-10-23 01:10:57 +0000

    parser

--- TrabGrafosFaa/src/FAA/TG/Entrada/Parser.java
+++ TrabGrafosFaa/src/FAA/TG/Entrada/Parser.java
@@ -5,28 +5,27 @@
 package FAA.TG.Entrada;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Parser {
-
-    public Parser() throws FileNotFoundException, IOException {
-
+    
+    public Parser(File selectedFile) {
         String linha = null;
         try {
             // instancia do arquivo que vou ler  
-            FileReader reader = new FileReader("mapeamento");
+            FileReader reader = new FileReader(selectedFile);
             BufferedReader leitor = new BufferedReader(reader);
 
             // loop que percorrerá todas as  linhas do arquivo.txt que eu quero ler  
             while ((linha = leitor.readLine()) != null) {
                 //No metodo StringTokenizer passo os parametros que quero ler, em seguida o que eu quero descartar no meu caso ( - ) e ( ; ).   
-                StringTokenizer st = new StringTokenizer(linha, "-;");
+                StringTokenizer st = new StringTokenizer(linha, "{}");
                 // Aqui determino que enquanto existir tokens que ele faça a leitura  
                 String dados = st.nextToken();
-
                 System.out.print(dados);
                 System.out.println("");
             }
@@ -37,5 +36,14 @@ public class Parser {
             e.printStackTrace();
 
         }
+    }
+    
+    public void ParserBFSDFS(){
+        
+        
+    }
+
+    public void ParserDijkstra() throws FileNotFoundException, IOException {
+
     }
 }
