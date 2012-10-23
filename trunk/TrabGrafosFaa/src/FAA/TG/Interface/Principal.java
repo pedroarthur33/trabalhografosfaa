e5919bf02780d18d30ffa5f65001e8431ed24e79
eduardohabitzreuter@gmail.com <eduardohabitzreuter@gmail.com@89243517-489e-33d1-b2b3-005f8cf9e537>
2012-10-23 01:10:57 +0000

    parser

--- trunk/TrabGrafosFaa/src/FAA/TG/Interface/Principal.java
+++ trunk/TrabGrafosFaa/src/FAA/TG/Interface/Principal.java
@@ -4,6 +4,10 @@
  */
 package FAA.TG.Interface;
 
+import FAA.TG.Entrada.Parser;
+import java.io.File;
+import javax.swing.JFileChooser;
+
 /**
  *
  * @author Eduardo
@@ -26,7 +30,33 @@ public class Principal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jMenu1.setText("Arquivo");
+
+        jMenuItem1.setText("Carregar Arquivo");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem1);
+
+        jMenuItem2.setText("Sair");
+        jMenu1.add(jMenuItem2);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Edit");
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -36,11 +66,21 @@ public class Principal extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 279, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        
+        JFileChooser arquivo = new JFileChooser();
+        arquivo.showOpenDialog(jMenu1);
+        String teste = arquivo.getSelectedFile().getAbsoluteFile().toString();
+        File arq =  new File(arquivo.getSelectedFile().getAbsoluteFile().toString());
+        Parser parser = new Parser(arq);
+        
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -84,5 +124,10 @@ public class Principal extends javax.swing.JFrame {
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
     // End of variables declaration//GEN-END:variables
 }
