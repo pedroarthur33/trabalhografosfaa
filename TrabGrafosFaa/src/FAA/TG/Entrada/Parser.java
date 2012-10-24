6caad8e237f3a7230a394d29b3500605a6eed1ac
eduardohabitzreuter@gmail.com <eduardohabitzreuter@gmail.com@89243517-489e-33d1-b2b3-005f8cf9e537>
2012-10-24 02:41:13 +0000

    Parser Funcionando.

--- TrabGrafosFaa/src/FAA/TG/Entrada/Parser.java
+++ TrabGrafosFaa/src/FAA/TG/Entrada/Parser.java
@@ -13,20 +13,33 @@ import java.util.StringTokenizer;
 
 public class Parser {
     
+    String [][] matriz = null;
+    
     public Parser(File selectedFile) {
         String linha = null;
+        String[] temp = null;
         try {
             // instancia do arquivo que vou ler  
             FileReader reader = new FileReader(selectedFile);
             BufferedReader leitor = new BufferedReader(reader);
-
+            int i = 0;
+            int j = 0;
             // loop que percorrerá todas as  linhas do arquivo.txt que eu quero ler  
             while ((linha = leitor.readLine()) != null) {
+                
                 //No metodo StringTokenizer passo os parametros que quero ler, em seguida o que eu quero descartar no meu caso ( - ) e ( ; ).   
                 StringTokenizer st = new StringTokenizer(linha, "{}");
+                int tamanho = st.countTokens();
                 // Aqui determino que enquanto existir tokens que ele faça a leitura  
                 String dados = st.nextToken();
                 System.out.print(dados);
+                temp = dados.split("\\,");
+                if (matriz == null) {matriz = new String[temp.length][temp.length];}
+                for(j = 0; j < temp.length; j++){
+                    matriz[i][j] = temp[j];
+                }
+                j = 0;
+                i++;
                 System.out.println("");
             }
             leitor.close();
